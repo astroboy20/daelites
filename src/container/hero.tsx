8525f2dd9f9b6c509b6429098c57057124834c8c
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-07-20 19:20:19 +0100

    feat: added animation to the hero section

--- src/container/hero.tsx
+++ src/container/hero.tsx
@@ -1,11 +1,24 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <div className="flex justify-between  mt-[92px] px-[6%] bg-[#F4FFE5]">
-      <div className="flex flex-col gap-5 mt-[90px]">
+    <div className="flex justify-between mt-[92px] pt-[6%] px-[5%] bg-[#F4FFE5]">
+      <motion.div
+        className="flex flex-col gap-5 mt-[90px]"
+        initial={{ x: "-100vw" }}
+        animate={{ x: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 50,
+          damping: 20,
+          duration: 1.0,
+          delay: 0.2,
+        }}
+      >
         <h1 className="text-[#0B9444] text-[40px] text-wrap font-[700]">
           Empower Your Future <br /> with Data Analytics <br /> Mastery
         </h1>
@@ -17,13 +30,25 @@ const Hero = () => {
         <Button className="text-[#fff] bg-[#8CC63F] rounded-[50px] py-[15px] px-[30px] text-[16px] font-[600] w-fit  hover:bg-transparent border-2 border-[#8CC63F] hover:text-[#8CC63F]">
           Get Started
         </Button>
-      </div>
-      <Image
-        src={"/images/hero.png"}
-        width={745}
-        height={745}
-        alt="hero-image"
-      />
+      </motion.div>
+      <motion.div
+        initial={{ y: "100vh" }}
+        animate={{ y: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 50,
+          damping: 20,
+          duration: 1.0,
+          delay: 0.2,
+        }}
+      >
+        <Image
+          src={"/images/hero.png"}
+          width={745}
+          height={745}
+          alt="hero-image"
+        />
+      </motion.div>
     </div>
   );
 };
