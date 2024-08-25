72595ba35cc25954a1628685bd1dd256a3973150
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-08-25 12:32:20 +0100

    feat: update

--- src/container/blog/full-blog.tsx
+++ src/container/blog/full-blog.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import Image from "next/image";
 import { urlFor } from "@/app/lib/sanity";
 import { PortableText } from "next-sanity";
+import { Button } from "@/components/ui/button";
 
 type fullBlogProp = {
   data: fullBlogType;
 };
 const FullBlog = ({ data }: fullBlogProp) => {
   return (
-    <main className="mt-[120px]">
+    <main className="mt-[120px] mx-[12%]">
       <div className="flex flex-col gap-10">
         <h1 className="tracking-tight text-[50px] font-[700] leading-[60px] text-center">
           {data?.title}
@@ -24,8 +25,13 @@ const FullBlog = ({ data }: fullBlogProp) => {
             className="rounded-[10px]"
           />
         </div>
-        <div className="prose">
-            <PortableText value={data.content}/>
+        <div className=" prose-[20px] ">
+          <PortableText value={data.content}/>
+        </div>
+        <div>
+            <Button className="text-[#107C41] text-[20px]">
+                Back
+            </Button>
         </div>
       </div>
     </main>
